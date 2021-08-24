aca5970119cb1bc48fb38170ac1e72d958521f57
TatianaThornton <tthornton@interfaz.io>
2021-08-24 09:09:37 -0600

    OrderController y eliminacion de Merchant

--- Training_BE_PS/src/main/java/io/interfaz/training/controllers/OrdersConroller.java
+++ Training_BE_PS/src/main/java/io/interfaz/training/controllers/OrdersConroller.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.interfaz.training.entities.Orders;
+import io.interfaz.training.entities.Products;
 import io.interfaz.training.services.OrdersService;
 
 @RestController
@@ -36,18 +38,24 @@ public class OrdersConroller {
 	public void deleteOrder(@PathVariable int id) {
 		ordersService.deleteOrder(id);
 	}
-	/*@PutMapping("/orders/{id}")
+	
+	@PutMapping("/orders/{id}")
 	public Orders replaceOrder(@RequestBody Orders newOrder, @PathVariable int id) {
 		return ordersService.getById(id)
-				.map(order->{
-					order.setPurchaseDate(newOrder.getPurchaseDate());
+				.map(order ->{
+					order.setCustomerId(newOrder.getCustomerId());
+					order.setDetails(newOrder.getDetails());
 					order.setIva(newOrder.getIva());
+					order.setPurchaseDate(newOrder.getPurchaseDate());
 					order.setSubtotal(newOrder.getSubtotal());
 					order.setTotal(newOrder.getTotal());
+					return ordersService.createOrder(order);
 				})
 				.orElseGet(()->{
 					newOrder.setId(id);
 					return ordersService.createOrder(newOrder);
 				});
-	}*/
+	}
+	
+	
 }
