2841ce2e39d74a67ecb5a1ac2affc323adffd27e
Victor Bolaños <vbolanos@interfaz.io>
2021-08-24 15:02:39 -0600

    Arreglos service y controller

--- src/main/java/io/interfaz/training/services/ProductsService.java
+++ src/main/java/io/interfaz/training/services/ProductsService.java
@@ -18,23 +18,32 @@ import io.interfaz.training.repos.ProductsRepository;
 public class ProductsService {
 	@Autowired
 	private ProductsRepository productsRespository;
-	
+
 	public Products createProduct(Products products) {
-		
 		return productsRespository.save(products);
 	}
-	
-	public List<Products> getAll(){
+
+	public Products updateProduct(Products productsRequest, int identifier) {
+		return getById(identifier).map(product -> {
+			product.setDescription(productsRequest.getDescription());
+			product.setPrice(productsRequest.getPrice());
+			product.setStatus(productsRequest.getStatus());
+			return createProduct(product);
+		}).orElseGet(() -> {
+			return createProduct(productsRequest);
+		});
+	}
+
+	public List<Products> getAll() {
 		return (List<Products>) productsRespository.findAll();
 	}
-	
+
 	public Optional<Products> getById(int id) {
 		return productsRespository.findById(id);
 	}
-	
+
 	public void deleteProduct(int id) {
 		productsRespository.deleteById(id);
 	}
 
-	
 }
